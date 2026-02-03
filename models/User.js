47c181efa2891bbd5bfaa917f37e3ea38d1299b2
Jordan Leturgez <jordanleturgez@gmail.com>
2026-02-03 03:13:48 -0500

    Add User model with authentication methods

---
+++ models/User.js
@@ -0,0 +1,54 @@
+// models/User.js
+const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
+
+const UserSchema = new mongoose.Schema({
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true,
+    lowercase: true
+  },
+  password: {
+    type: String,
+    required: function() {
+      return !this.twitterId; // Only required if not using Twitter auth
+    }
+  },
+  twitterId: {
+    type: String,
+    sparse: true,
+    unique: true
+  },
+  displayName: {
+    type: String,
+    required: true
+  },
+  goingTo: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Venue'
+  }],
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+UserSchema.pre('save', async function(next) {
+  if (!this.isModified('password') || !this.password) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (err) {
+    next(err);
+  }
+});
+
+UserSchema.methods.comparePassword = async function(candidatePassword) {
+  if (!this.password) return false;
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+module.exports = mongoose.model('User', UserSchema);
